7f6cba540080b37f640d8bf217a990953c5b4152
Tedros <tedtfu@gmail.com>
2020-09-08 15:17:37 -0400

    directory change

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React ,{useRef} from 'react';
 import { useHistory} from 'react-router-dom';
-import './app.css';
+import '../app.css';
 
 export default function Home() {
   const history=useHistory();
